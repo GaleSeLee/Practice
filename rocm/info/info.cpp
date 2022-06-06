eb9aa5728a6fbb5e4a8b88e9aab54b2db879f5cb
zeyuli <li_zeyu@pku.edu.cn>
2022-06-06 17:43:25 +0800

    add hip info

---
+++ rocm/info/info.cpp
@@ -0,0 +1,21 @@
+#include <hip/hip_runtime.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include "error.hpp"
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    hipDeviceProp_t prop;
+    int count;
+    hipGetDeviceCount(&count);
+
+    for(int ii = 0; ii < count; ii++) {
+        hipGetDeviceProperties(&prop ,ii);
+        cout << prop.name << endl;
+	cout << prop.totalGlobalMem/1024/1024 << endl;
+    }
+
+    return 0;
+}
